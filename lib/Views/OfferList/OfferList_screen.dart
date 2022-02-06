c7c8ae47c6632c6012506f4b92a1e9f8baccc03e
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-02-06 13:23:39 -0400

    creacion de maqueta sencilla
    se creo la pantalla pero de manera sencilla para tener un prototipo

---
+++ lib/Views/OfferList/OfferList_screen.dart
@@ -0,0 +1,39 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OfferList extends StatefulWidget {
+  @override
+  _OfferListState createState() => _OfferListState();
+}
+
+class _OfferListState extends State<OfferList> {
+  List<String> list_Of_Offers = <String>['Prueba1', 'Prueba2', 'Prueba3'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('OfferList'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: /* Column(
+        children: <Widget>[*/
+          ListView.builder(
+              itemCount: list_Of_Offers.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.amber[200],
+                  child: ListTile(
+                    title: Text(list_Of_Offers[index]),
+                  ),
+                );
+              }
+              //],
+              ),
+    );
+  }
+}
